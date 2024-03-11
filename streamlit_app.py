51f42703556a43c5f287f94a21f124714d8f93fc
Daniel Verduzco <111399164+DanielVerduzco6@users.noreply.github.com>
2024-03-11 15:45:47 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,18 +67,6 @@ def recompensas(estado_final, estado_intermedio, num):
 
 # Elección del punto inicial, final e intermedios
 
-st.set_page_config(layout="wide", page_title="Ruta Óptima con Q-Learning", page_icon=None, initial_sidebar_state="collapsed")
-st.markdown(
-"""
-<style>
-    body {
-        background-color: #ffffff !important;
-    }
-</style>
-""",
-unsafe_allow_html=True
-)
-
 st.title('Ruta Óptima con Q-Learning')
 
 st.subheader('Equipo')
