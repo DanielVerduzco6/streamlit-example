89e1e3df846ac80cb31df617950bfae782d8be4c
Daniel Verduzco <111399164+DanielVerduzco6@users.noreply.github.com>
2024-04-24 16:17:53 -0600

    Update AST6BICI_app.py

--- AST6BICI_app.py
+++ AST6BICI_app.py
@@ -2,14 +2,14 @@ import streamlit as st
 
 # Portada principal
 st.write("# **Análisis de series temporales**")
-st.write("## **Actividad: Estadísticas descriptivas**")
+st.write("## **Compendio de actividades y ejercicios**")
 st.write("**Estudiante: Verduzco Valencia Daniel Alejandro**")
 st.write("**Profesor: Mata Lopez Walter Alexander**")
 st.write("**6.-B**")
 st.write("**Ingeniería en Computación Inteligente**")
 st.write("**Universidad de Colima**")
 st.write("**FIME**")
-st.write("**12/04/2024**")
+st.write("**26/04/2024**")
 st.image("https://www.ucol.mx/content/cms/41/image/escudos.png", use_column_width=True)
 
 # Menú desplegable para acceder a las actividades
@@ -18,6 +18,29 @@ actividad = st.selectbox("Seleccione una actividad:", ["Actividad 1", "Actividad
 if actividad == "Actividad 1":
     st.write('### Código 1: Estadísticas descriptivas')
     # Aquí va el código 1
+
+    st.markdown("""
+    <div align="center">
+        
+    # **Análisis de series temporales**
+    
+    ## **Actividad: Estadísticas descriptivas**
+    
+    **Estudiante: Verduzco Valencia Daniel Alejandro**
+    
+    **Profesor: Mata Lopez Walter Alexander**
+    
+    **6.-B**
+    
+    **Ingeniería en Computación Inteligente**  
+    **Universidad de Colima**  
+    **FIME**
+    
+    **12/04/2024**
+    
+    ![Logo de la Universidad de Colima](https://www.ucol.mx/content/cms/41/image/escudos.png)
+    </div>
+    """, unsafe_allow_html=True)
 elif actividad == "Actividad 2":
     st.write('### Código 2: Tarea de patrones')
     # Aquí va el código 2
