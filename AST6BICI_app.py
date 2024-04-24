dfa28acf080861c1afdf545289f6ff5ded328d2d
Daniel Verduzco <111399164+DanielVerduzco6@users.noreply.github.com>
2024-04-24 14:58:57 -0600

    Update AST6BICI_app.py

--- AST6BICI_app.py
+++ AST6BICI_app.py
@@ -1,37 +1,27 @@
 import streamlit as st
 
 # Portada principal
-st.title('Análisis de Series Temporales')
+st.title('Análisis de series temporales')
 st.header('Compendio de tareas y ejercicios')
-st.subheader('Estudiante: Verduzco Valencia Daniel Alejandro')
-st.subheader('Profesor: Mata López Walter Alexander')
-st.subheader('6-B')
-st.subheader('Ingeniería en Computación Inteligente')
-st.subheader('Universidad de Colima')
-st.subheader('FIME')
-st.subheader('2024')
+st.write("Estudiante: Verduzco Valencia Daniel Alejandro")
+st.write("Profesor: Mata López Walter Alexander")
+st.write("6-B")
+st.write("Ingeniería en Computación Inteligente")
+st.write("Universidad de Colima")
+st.write("FIME")
+st.write("2024")
 
-# Botones para acceder a los parciales
+# Botones para acceder a los códigos
 if st.button('Parcial 1 y 2'):
-    st.header('Parcial 1 y 2')
-    st.subheader('Código 1: Tarea de Anomalías')
-    st.code("""
-    # Aquí va el código de la Tarea de Anomalías
-    """, language='python')
+    st.write('### Código 1: Estadísticas descriptivas')
+    # Aquí va el código 1
+    
+    st.write('### Código 2: Tarea de patrones')
+    # Aquí va el código 2
+    
+    st.write('### Código 3: Tarea de Anomalías')
+    # Aquí va el código 3
+    
+    st.write('### Código 4: Análisis de Estacionariedad de una Serie Temporal')
+    # Aquí va el código 4
 
-    st.subheader('Código 2: Análisis de Estacionariedad de una Serie Temporal')
-    st.code("""
-    # Aquí va el código de Análisis de Estacionariedad de una Serie Temporal
-    """, language='python')
-
-if st.button('Parcial 3'):
-    st.header('Parcial 3')
-    st.subheader('Código 3: Tarea de Anomalías')
-    st.code("""
-    # Aquí va el código de la Tarea de Anomalías
-    """, language='python')
-
-    st.subheader('Código 4: Análisis de Estacionariedad de una Serie Temporal')
-    st.code("""
-    # Aquí va el código de Análisis de Estacionariedad de una Serie Temporal
-    """, language='python')
