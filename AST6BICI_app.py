a708d43edb6eb4c94fe3c3439ddf7ec4f40db13b
Daniel Verduzco <111399164+DanielVerduzco6@users.noreply.github.com>
2024-04-24 16:08:03 -0600

    Update AST6BICI_app.py

--- AST6BICI_app.py
+++ AST6BICI_app.py
@@ -1,6 +1,3 @@
-import streamlit as st
-
-# Portada principal
 # Portada principal
 st.write("# **Análisis de series temporales**")
 st.write("## **Actividad: Estadísticas descriptivas**")
@@ -13,16 +10,18 @@ st.write("**FIME**")
 st.write("**12/04/2024**")
 st.image("https://www.ucol.mx/content/cms/41/image/escudos.png", use_column_width=True)
 
-# Botones para acceder a los códigos
-if st.button('Actividad 1'):
+# Menú desplegable para acceder a las actividades
+actividad = st.selectbox("Seleccione una actividad:", ["Actividad 1", "Actividad 2", "Actividad 3", "Actividad 4"])
+
+if actividad == "Actividad 1":
     st.write('### Código 1: Estadísticas descriptivas')
     # Aquí va el código 1
-elif st.button('Actividad 2'):
+elif actividad == "Actividad 2":
     st.write('### Código 2: Tarea de patrones')
     # Aquí va el código 2
-elif st.button('Actividad 3'):
+elif actividad == "Actividad 3":
     st.write('### Código 3: Tarea de Anomalías')
     # Aquí va el código 3
-elif st.button('Actividad 4'):
+elif actividad == "Actividad 4":
     st.write('### Código 4: Análisis de Estacionariedad de una Serie Temporal')
     # Aquí va el código 4
