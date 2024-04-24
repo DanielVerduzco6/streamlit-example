23505b9870bda5a55252b5341bdfd4ad71b2d45a
Daniel Verduzco <111399164+DanielVerduzco6@users.noreply.github.com>
2024-04-24 16:04:16 -0600

    Update AST6BICI_app.py

--- AST6BICI_app.py
+++ AST6BICI_app.py
@@ -1,26 +1,36 @@
 import streamlit as st
 
 # Portada principal
-st.title('Análisis de series temporales')
-st.header('Compendio de tareas y ejercicios')
-st.write("Estudiante: Verduzco Valencia Daniel Alejandro")
-st.write("Profesor: Mata López Walter Alexander")
-st.write("6-B")
-st.write("Ingeniería en Computación Inteligente")
-st.write("Universidad de Colima")
-st.write("FIME")
-st.write("2024")
+    st.markdown("""
+    # **Análisis de series temporales**
+    
+    ## **Actividad: Estadísticas descriptivas**
+    
+    **Estudiante: Verduzco Valencia Daniel Alejandro**
+    
+    **Profesor: Mata Lopez Walter Alexander**
+    
+    **6.-B**
+    
+    **Ingeniería en Computación Inteligente**  
+    **Universidad de Colima**  
+    **FIME**
+    
+    **12/04/2024**
+    
+    ![Logo de la Universidad de Colima](https://www.ucol.mx/content/cms/41/image/escudos.png)
+    """)
 
 # Botones para acceder a los códigos
-if st.button('Parcial 1 y 2'):
+if st.button('Actividad 1'):
     st.write('### Código 1: Estadísticas descriptivas')
     # Aquí va el código 1
-    
+elif st.button('Actividad 2'):
     st.write('### Código 2: Tarea de patrones')
     # Aquí va el código 2
-    
+elif st.button('Actividad 3'):
     st.write('### Código 3: Tarea de Anomalías')
     # Aquí va el código 3
-    
+elif st.button('Actividad 4'):
     st.write('### Código 4: Análisis de Estacionariedad de una Serie Temporal')
     # Aquí va el código 4
